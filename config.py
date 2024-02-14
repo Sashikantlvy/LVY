8f62db09ac776901c5f465f7c8937c304d6ea51a
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-14 09:06:07 +0530

    Update config.py
    Signed-off-by: Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Sashikantlvy/PRIVATE",
+    "https://github.com/Sashikantlvy/XMusic2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
