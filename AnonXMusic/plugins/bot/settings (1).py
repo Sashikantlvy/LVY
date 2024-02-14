1dfdb6be7bf10e3f3b450fdcae72a382cd0c60cf
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-14 11:57:35 +0530

    Update settings (1).py
    Signed-off-by: Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>

--- AnonXMusic/plugins/bot/settings (1).py
+++ AnonXMusic/plugins/bot/settings (1).py
@@ -90,19 +90,15 @@ async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
         )
 
 
-@app.on_callback_query(filters.regex("gib_source"))
-async def gib_repo_callback(_, callback_query):
-    await callback_query.edit_message_media(
-        media=InputMediaVideo("https://telegra.ph/file/bb5db43a2d9d71954200f.mp4", has_spoiler=True),
+@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
+@languageCB
+async def gib_repo(client, CallbackQuery, _):
+    await CallbackQuery.edit_message_media(
+        InputMediaVideo("https://graph.org/file/69b3f128014d53bca943a.mp4"),
         reply_markup=InlineKeyboardMarkup(
-            [
-                [close_button]
-            ]
+            [[InlineKeyboardButton(text="✭ʙᴀᴄᴋ✭", callback_data=f"settingsback_helper")]]
         ),
     )
-
-close_button = InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="close")
-
 
 
 @app.on_callback_query(
